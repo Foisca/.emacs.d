e13d102857df84470e14a8966f9452cf16e3a14e
Fosica <Yjy09062000@outlook.com>
2020-09-24 18:40:50 +0800

    first

---
+++ lisp/init-const.el
@@ -0,0 +1,7 @@
+;;judge operating system
+(defconst *is-mac* (eq system-type 'darwin))
+(defconst *is-linux* (eq system-type 'gnu/linux))
+(defconst *is-windows* (or (eq system-type 'ms-dos) (eq system-type 'windows-nt)))
+
+(provide 'init-const)
+
---
+++ lisp/init-kbd.el~
@@ -0,0 +1,11 @@
+
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+(defalias 'C-b 'C-h)
+
+(setq indent-tabs-mode nil)
+(setq c-default-style "linux")
+(setq c-basic-offset 4)
+(setq default-tab-width 4)
+
+(provide 'init-kbd)
